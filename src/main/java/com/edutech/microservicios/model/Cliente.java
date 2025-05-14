a35053a194b1a863414ba45a7209c0697f32e213
Menji-0 <matkingmc98@gmail.com>
2025-05-14 16:53:37 -0400

    Agregado codigo al Modelo

--- src/main/java/com/edutech/microservicios/model/Cliente.java
+++ src/main/java/com/edutech/microservicios/model/Cliente.java
@@ -1,4 +1,35 @@
 package com.edutech.microservicios.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name="cliente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String clave;
+
+    @Column(nullable = false)
+    private String nombres;
+
+    @Column(nullable = false)
+    private String apellidos;
+
+    @Column(nullable = true)
+    private String fechaNacimiento;
+
+    @Column(nullable = false)
+    private String correo;
 }
