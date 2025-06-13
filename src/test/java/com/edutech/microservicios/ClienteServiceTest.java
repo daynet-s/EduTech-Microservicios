9dfd39a2fca7717f469d85f08340ae4fc25ec853
daynet-s <nuhuh@mail.com>
2025-06-13 17:00:04 -0400

    - Cambios a ClienteServiceTest para pruebas JUnit.

--- src/test/java/com/edutech/microservicios/ClienteServiceTest.java
+++ src/test/java/com/edutech/microservicios/ClienteServiceTest.java
@@ -2,35 +2,35 @@ package com.edutech.microservicios;
 // MODIFICAR PARA TESTS JUNIT
 import com.edutech.microservicios.model.Cliente;
 import com.edutech.microservicios.repository.ClienteRepository;
+import com.edutech.microservicios.service.ClienteService;
+import org.junit.jupiter.api.Test;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
-@Service
-@Transactional
+@SpringBootTest
 public class ClienteServiceTest {
+
     @Autowired
+    private ClienteService clienteServ;
+
+    @MockBean
     private ClienteRepository clienteRepo;
 
-    // Encontrar todas las entidades
-    public List<Cliente> findAll() {
-        return clienteRepo.findAll();
-    }
+    // REVISAR POR QUE LOS METODOS DA ERROR EN COMPARACION A DOC
+    /*@Test
+    public void testFindAll() {
+        when(clienteRepo.findAll()).thenReturn(List.of(new Cliente(1, "ashe12", "Juan", "Perez",
+                                                                LocalDate.now(), "juan@correo.com")));
+        List<Cliente> clientes = clienteServ.findAll();
 
-    // Encontrar por Id
-    public Cliente findById (long id) {
-        return clienteRepo.findById(id).get();
-    }
-
-    // Guardar entidad
-    public Cliente save(Cliente cliente) {
-        return clienteRepo.save(cliente);
-    }
-
-    // Eliminar entidad
-    public void delete(Long id) {
-        clienteRepo.deleteById(id);
-    }
+        assertNotNull(clientes);
+        assertEquals(1, clientes.size());
+    }*/
 }
